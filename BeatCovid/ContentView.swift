3ff61f410ec5f212f0fc79e8628ef180121df502
ThomasKwiatkowska <spartandawg2019@gmail.com>
2022-02-02 14:31:06 -0500

    Made text red

--- BeatCovid/ContentView.swift
+++ BeatCovid/ContentView.swift
@@ -7,12 +7,17 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
     var body: some View {
         Text("Hello, PM!")
+            .foregroundColor(.red)
             .padding()
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
