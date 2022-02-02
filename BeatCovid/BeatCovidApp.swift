7bf2c0d7d20bf558c54b831c7ef700cc465336f9
ThomasKwiatkowska <spartandawg2019@gmail.com>
2022-02-02 14:00:46 -0500

    Initial Commit

---
+++ BeatCovid/BeatCovidApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeatCovidApp.swift
+//  BeatCovid
+//
+//  Created by Tom K on 2/2/22.
+//
+
+import SwiftUI
+
+@main
+struct BeatCovidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
